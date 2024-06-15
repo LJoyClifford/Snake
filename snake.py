de1e611df262bf07372c038b4de373a5cf46360d
Lisa Clifford <78510843+LJoyClifford@users.noreply.github.com>
2024-06-15 19:24:37 -0400

    add snake movement

--- snake.py
+++ snake.py
@@ -30,21 +30,21 @@ font = pygame.font.SysFont('gabriola', 48)
 
 title_text = font.render("~Snake~", True, GREEN, RED)
 title_rect = title_text.get_rect()
-title_react.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) # type: ignore
+title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) # type: ignore
 
 score_text = font.render("Score: " + str(score), True, GREEN, RED)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)
 
 game_over_text = font.render("GAMEOVER", True, GREEN, RED)
-game_over_rect = game_over_text.get_react()
+game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT//2)
 
 continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
 continue_rect = continue_text.get_rect()
 continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
 
-pick_up_sound = pygame.mixer.Sound("pick_up_sound.wav")
+#pick_up_sound = pygame.mixer.Sound("pick_up_sound.wav")
 
 #set image rectangles
 apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
@@ -62,6 +62,33 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+            #move the snake
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                snake_dx = -1 * SNAKE_SIZE
+                snake_dy = 0 
+            if event.key == pygame.K_RIGHT:
+                snake_dx = SNAKE_SIZE
+                snake_dy = 0
+            if event.key == pygame.K_UP:
+                snake_dx = 0
+                snake_dy = -1 * SNAKE_SIZE
+            if event.key == pygame.K_DOWN:
+                snake_dx = 0
+                snake_dy = SNAKE_SIZE
+
+#update position of snakes head
+    head_x += snake_dx
+    head_y += snake_dy
+    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+
+    #check for collisions
+
+
+
+
+    
     display_surface.fill(WHITE)
 
     display_surface.blit(title_text, title_rect)
@@ -77,3 +104,6 @@ while running:
 
 #end
 pygame.quit()
+
+
+
