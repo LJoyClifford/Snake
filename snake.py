a12b100fddf386e8c1a3b9c12076bac6bde59ef1
Lisa Clifford <78510843+LJoyClifford@users.noreply.github.com>
2024-06-15 20:38:14 -0400

    update font and colors

--- snake.py
+++ snake.py
@@ -22,26 +22,25 @@ snake_dy = 0
 
 score = 0
 
-GREEN = (0, 255, 0)
-DARKGREEN = (10, 50, 10)
+BLACK = (0, 0, 0)
 RED = (150, 0, 0)
 WHITE = (255, 255, 255)
 
-font = pygame.font.SysFont('gabriola', 48)
+font = pygame.font.SysFont('arial', 48)
 
-title_text = font.render("~Snake~", True, GREEN, RED)
+title_text = font.render("~Snake~", True, BLACK, RED)
 title_rect = title_text.get_rect()
 title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) # type: ignore
 
-score_text = font.render("Score: " + str(score), True, GREEN, RED)
+score_text = font.render("Score: " + str(score), True, BLACK, RED)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)
 
-game_over_text = font.render("GAMEOVER", True, GREEN, RED)
+game_over_text = font.render("GAMEOVER", True, BLACK, RED)
 game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT//2)
 
-continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
+continue_text = font.render("Press any key to play again", True, RED, BLACK)
 continue_rect = continue_text.get_rect()
 continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
 
@@ -50,7 +49,7 @@ apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
 apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
 
 head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
-head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
+head_rect = pygame.draw.rect(display_surface, BLACK, head_coord)
 
 body_coords = []
 
@@ -123,7 +122,7 @@ while running:
         body_coords.append(head_coord)
 
     #update score
-    score_text = font.render("Score: " + str(score), True, GREEN, RED)
+    score_text = font.render("Score: " + str(score), True, BLACK, RED)
 
 
     display_surface.fill(WHITE)
@@ -132,8 +131,8 @@ while running:
     display_surface.blit(score_text, score_rect)
 
     for body in body_coords:
-        pygame.draw.rect(display_surface, DARKGREEN, body)
-    head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
+        pygame.draw.rect(display_surface, BLACK, body)
+    head_rect = pygame.draw.rect(display_surface, BLACK, head_coord)
     apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
 
 #update display clock
